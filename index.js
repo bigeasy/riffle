daa1bc10f22ba3168c4daa2f43002aaca3e2d38f
Alan Gutierrez <alan@prettyrobots.com>
2013-12-17 19:20:09 -0800

    Rough sketch of a forward iterator.
    See #5.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-module.exports = 1
+var cadence = require('cadence')
+
+function yes () { return true }
+
+function Forward (cursor) {
+    this._cursor = cursor
+}
+
+Forward.prototype.next = cadence(function (step, condition) {
+    condition = condition || yes
+    step(null)
+})
+
+Forward.prototype.unlock = function () {
+    this._cursor.unlock()
+}
+
+exports.forward = cadence(function (step, strata, key) {
+    var condition = key == null ? strata.left : strata.key(key)
+    step(function () {
+        strata.iterator(condition, step())
+    }, function (cursor) {
+        return new Forward(cursor)
+    })
+})
--- t/riffle/forward.t.js
+++ t/riffle/forward.t.js
@@ -1,4 +1,28 @@
-require('./proof')(1, function (ok) {
-    require('../..')
-    ok(1, 'harness')
+require('./proof')(1, function (step, serialize, deepEqual, Strata, tmp) {
+    var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 }),
+        riffle = require('../..')
+    step(function () {
+        serialize(__dirname + '/fixtures/nine.json', tmp, step())
+    }, function () {
+        strata.open(step())
+    }, function () {
+        var forward = riffle.forward(strata, 'a', step())
+    }, function (iterator) {
+        var records = []
+        step(function () {
+            step(function () {
+                iterator.next(step())
+            }, function (record) {
+                if (record) records.push(record)
+                else step(null, records)
+            })()
+        }, function () {
+            iterator.unlock()
+            strata.close(step())
+        }, function () {
+            return records
+        })
+    }, function (records) {
+        deepEqual([], records, 'records')
+    })
 })
