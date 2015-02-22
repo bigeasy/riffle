fba5266e21fe24c0183ba535f8d2ced8232c2cb7
Alan Gutierrez <alan@prettyrobots.com>
2015-02-22 01:59:51 -0600

    Coverage of reverse key race.
    Closes #75.

--- t/riffle/reverse.t.js
+++ t/riffle/reverse.t.js
@@ -128,5 +128,60 @@ function prove (async, assert) {
         })
     }, function () {
         strata.close(async())
+    }, function () {
+        serialize(__dirname + '/fixtures/nine.json', tmp, async())
+    }, function () {
+        strata.open(async())
+    }, function () {
+        riffle._racer = cadence(function (async, key) {
+            if (key == 'h') async(function () {
+                strata.mutator('h', async())
+            }, function (cursor) {
+                async(function () {
+                    cursor.remove(cursor.index)
+                    cursor.unlock(async())
+                })
+            }, function () {
+                strata.balance(async())
+            }, function () {
+                strata.mutator('h', async())
+            }, function (cursor) {
+                async(function () {
+                    cursor.insert('h', 'h', ~cursor.index)
+                    cursor.unlock(async())
+                })
+            }, function () {
+                strata.balance(async())
+            }, function () {
+                strata.mutator('i', async())
+            }, function (cursor) {
+                async(function () {
+                    cursor.remove(cursor.index)
+                    cursor.unlock(async())
+                })
+            }, function () {
+                strata.balance(async())
+            })
+        })
+        riffle.reverse(strata, async())
+    }, function (iterator) {
+        var records = []
+        async(function () {
+            var loop = async(function () {
+                iterator.next(async())
+            }, function (more) {
+                if (more) {
+                    var item
+                    while (item = iterator.get()) {
+                        records.push(item.record)
+                    }
+                } else {
+                    return [ loop ]
+                }
+            })()
+        }, function () {
+            assert(records, [ 'i', 'h', 'g', 'f', 'd', 'c', 'b', 'a' ], 'key re-inserted')
+            iterator.unlock(async())
+        })
     })
 }
